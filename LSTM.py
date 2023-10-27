b2f7d404a6e320c23ea498ff3e6b56803083a144
YuwenZhou99 <144034975+YuwenZhou99@users.noreply.github.com>
2023-10-27 13:37:20 +0200

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -14,27 +14,26 @@ from tensorflow.keras.layers import TextVectorization, Bidirectional
 from tensorflow.keras.utils import to_categorical
 import tensorflow as tf
 
-# Maximize reproducibility by setting random seeds
+# Make reproducible as much as possible
 np.random.seed(1234)
 tf.random.set_seed(1234)
 python_random.seed(1234)
 
 def create_arg_parser():
-    # Create a command-line argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--train_file", default='train.tsv', type=str,
-                        help="Input file for learning")
+                        help="Input file to learn from (default train.tsv)")
     parser.add_argument("-d", "--dev_file", type=str, default='dev.tsv',
-                        help="Separate development set to read")
+                        help="Separate dev set to read in (default dev.tsv)")
     parser.add_argument("-t", "--test_file", type=str, default='test.tsv',
-                        help="If provided, use the trained model to predict the test set")
+                        help="If added, use trained model to predict on test set (default test.tsv)")
     parser.add_argument("-e", "--embeddings", default='glove.twitter.27B.200d.txt', type=str,
-                        help="Embedding file to use")
+                        help="Embedding file we are using (default glove.twitter.27B.200d.txt)")
     args = parser.parse_args()
     return args
 
 def read_corpus(corpus_file):
-    # Read a corpus file and separate features and labels
+    '''Read in review data set and returns docs and labels'''
     documents = []
     labels = []
     with open(corpus_file, encoding='utf-8') as in_file:
@@ -45,7 +44,7 @@ def read_corpus(corpus_file):
     return documents, labels
 
 def read_embeddings(embeddings_file):
-    # Read embeddings from a file and save them as a dictionary
+    '''Read in word embeddings from file and save as numpy array'''
     embeddings = {}
     with open(embeddings_file, 'r', encoding='UTF-8') as file:
         for line in file:
@@ -56,6 +55,7 @@ def read_embeddings(embeddings_file):
     return embeddings
 
 def get_emb_matrix(vocab, emb):
+    '''Get embedding matrix given vocab and the embeddings'''
     # Get an embedding matrix based on vocabulary and embeddings
     num_tokens = len(vocab) + 2
     word_index = dict(zip(vocab, range(len(vocab))))
@@ -68,8 +68,9 @@ def get_emb_matrix(vocab, emb):
     return embedding_matrix
 
 def create_model(Y_train, emb_matrix):
-    # Define model hyperparameters
-    learning_rate = 0.0005
+    '''Create the Keras model to use'''
+    # Define model hyperparameters, you can fine-tune them here
+    learning_rate = 5e-4
     loss_function = 'binary_crossentropy'
     optim = Adam(learning_rate=learning_rate)
     embedding_dim = len(emb_matrix[0])
@@ -90,19 +91,23 @@ def create_model(Y_train, emb_matrix):
     return model
 
 def train_model(model, X_train, Y_train, X_dev, Y_dev):
-    Y_train = to_categorical(Y_train)  # Transform training set labels
-    Y_dev = to_categorical(Y_dev)  # Transform development set labels
+    '''Train the model here'''
+    # Transform set labels
+    Y_train = to_categorical(Y_train) 
+    Y_dev = to_categorical(Y_dev) 
 
-    # Train the model
+    # You can fine-tune the batch size and epochs here
     verbose = 1
     batch_size = 64
-    epochs = 12
-    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+    epochs = 50
+    # Early stopping: stop training when there are two consecutive epochs without improving
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=2)
     model.fit(X_train, Y_train, verbose=verbose, epochs=epochs, callbacks=[callback], batch_size=batch_size, validation_data=(X_dev, Y_dev))
     test_set_predict(model, X_dev, Y_dev, "dev")
     return model
 
 def test_set_predict(model, X_test, Y_test, identifier):
+    '''Do predictions and measure accuracy on our own test set (that we split off train)'''
     # Use the trained model for prediction
     Y_pred = model.predict(X_test)
     Y_pred = np.argmax(Y_pred, axis=1)
@@ -120,20 +125,18 @@ def test_set_predict(model, X_test, Y_test, identifier):
     print('Accuracy on our own {} set: {}'.format(identifier, round(accuracy_score(Y_test, Y_pred), 3)))
 
 def main():
-    # Parse command-line arguments
+    '''Main function to train and test neural network given cmd line arguments'''
     args = create_arg_parser()
 
-    # Read training and development data
+    # Read in the data and embeddings
     X_train, Y_train = read_corpus(args.train_file)
     X_dev, Y_dev = read_corpus(args.dev_file)
-
-    # Read word embeddings
     embeddings = read_embeddings(args.embeddings)
 
-    # Initialize a text vectorizer for processing text data
+    # Transform words to indices using a vectorizer
     vectorizer = TextVectorization(standardize=None, output_sequence_length=50)
 
-    # Create a TensorFlow dataset from training and development data
+    # Use train and dev to create vocab - could also do just train
     text_ds = tf.data.Dataset.from_tensor_slices(X_train + X_dev)
 
     # Adapt the vectorizer to the data to build a vocabulary
@@ -146,7 +149,7 @@ def main():
     # Initialize a label binarizer to transform categorical labels to binary format
     encoder = LabelBinarizer()
 
-    # Transform training and development labels into binary format
+    # Transform string labels to one-hot encodings
     Y_train_bin = encoder.fit_transform(Y_train)
     Y_dev_bin = encoder.transform(Y_dev)
 
@@ -160,11 +163,12 @@ def main():
     # Train the model
     model = train_model(model, X_train_vect, Y_train_bin, X_dev_vect, Y_dev_bin)
 
-    # If a test file is provided, read the test data and evaluate the model
+    # Do predictions on specified test set
     if args.test_file:
         X_test, Y_test = read_corpus(args.test_file)
         Y_test_bin = encoder.transform(Y_test)
         X_test_vect = vectorizer(np.array([[s] for s in X_test])).numpy()
+        Y_test_bin = to_categorical(Y_test_bin)
         test_set_predict(model, X_test_vect, Y_test_bin, "test")
 
 if __name__ == '__main__':
